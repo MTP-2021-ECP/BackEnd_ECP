b455025ce3af9cd196a451cdb455aa4609d3d313
AlexisResses <alexis.resses@epitech.eu>
2021-02-03 17:17:06 +0100

    register + mail de de bienvenue fonctionne en local avec front testalex

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,9 +16,9 @@ class Cors
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request);
         header('Access-Control-Allow-Origin:  http://localhost:8081');
         header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin');
         header('Access-Control-Allow-Methods:  POST, PUT');
+        return $next($request);
     }
 }
--- config/cors.php
+++ config/cors.php
@@ -14,7 +14,7 @@ return [
     | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
     |
     */
-    'paths' => ['api/*'],
+    'paths' => ['*,*'],
     'allowed_methods' => ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'],
     'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
